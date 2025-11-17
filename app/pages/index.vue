e094c0326ec154f0c9fbde45f69920f1ab4f65e3
Hazrat02 <hazratbd80@gmail.com>
2025-11-17 10:48:08 -0800

    nuxt 1st project

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<!-- pages/index.vue -->
+<template>
+  <div>
+    <h1>Welcome to CreatelizeSwap</h1>
+    <p>If you see this, Nuxt routing is working!</p>
+  </div>
+</template>
+
+<script setup>
+// you can add logic here later
+</script>
